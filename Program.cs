2ece29a127c61628dc11ba14778ad1e564115f5e
sksah <sksah1842@gmail.com>
2026-07-01 22:20:22 +0530

    grade calculator and methods

--- Program.cs
+++ Program.cs
@@ -7,64 +7,125 @@ using System.Threading.Tasks;
 namespace HelloWorld
 {
     internal class Program
-    {
+    {   
+
+        public static void Greet()
+        {
+            Console.WriteLine("Hello");
+        }
         static void Main(string[] args)
         {
-            Console.Write("Enter first number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter first number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter an operator (+, -, *, /, %): ");
-            char op = Convert.ToChar(Console.ReadLine());
+            //Console.Write("Enter an operator (+, -, *, /, %): ");
+            //char op = Convert.ToChar(Console.ReadLine());
 
-            Console.Write("Enter second number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter second number: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
 
-            double result = 0;
-            bool isOperationValid = true;
+            //double result = 0;
+            //bool isOperationValid = true;
 
-            if (op == '+')
-            {
-                result = num1 + num2;
-            }
-            if (op == '-')
-            {
-                result = num1 - num2;
-            }
-            if (op == '*')
-            {
-                result = num1 * num2;
-            }
+            //if (op == '+')
+            //{
+            //    result = num1 + num2;
+            //}
+            //if (op == '-')
+            //{
+            //    result = num1 - num2;
+            //}
+            //if (op == '*')
+            //{
+            //    result = num1 * num2;
+            //}
 
-            if (op == '/')
-            {
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                if (num2 == 0) 
-                {
-                    Console.WriteLine("Error: Cannot divide by zero.");
-                    isOperationValid = false;
-                }
-            } 
+            //if (op == '/')
+            //{
+            //    if (num2 != 0)
+            //    {
+            //        result = num1 / num2;
+            //    }
+            //    if (num2 == 0) 
+            //    {
+            //        Console.WriteLine("Error: Cannot divide by zero.");
+            //        isOperationValid = false;
+            //    }
+            //} 
 
-            if (op == '%')
-            {
-                result = num1 % num2;
-            }
+            //if (op == '%')
+            //{
+            //    result = num1 % num2;
+            //}
 
-            if (op != '+' && op != '-' && op != '*' && op != '/' && op != '%')
-            {
-                Console.WriteLine("Error: Invalid operator entered.");
-                isOperationValid = false;
-            }
+            //if (op != '+' && op != '-' && op != '*' && op != '/' && op != '%')
+            //{
+            //    Console.WriteLine("Error: Invalid operator entered.");
+            //    isOperationValid = false;
+            //}
 
-            if (isOperationValid == true)
-            {
-                Console.WriteLine("Result: " + num1 + " " + op + " " + num2 + " = " + result);
-            }
+            //if (isOperationValid == true)
+            //{
+            //    Console.WriteLine("Result: " + num1 + " " + op + " " + num2 + " = " + result);
+            //}
 
-            Console.ReadLine(); 
+            //Console.ReadLine(); 
+
+            // garde calculator
+            //Console.Write("Enter score for Assignment 1: ");
+            //double score1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter score for Assignment 2: ");
+            //double score2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter score for Assignment 3: ");
+            //double score3 = Convert.ToDouble(Console.ReadLine());
+
+            //double totalScore = score1 + score2 + score3;
+            //double average = totalScore / 3;
+
+            //char letterGrade = 'F';
+            //bool passed = true;
+
+            //if (average >= 90)
+            //{
+            //    letterGrade = 'A';
+            //}
+            //if (average >= 80 && average < 90)
+            //{
+            //    letterGrade = 'B';
+            //}
+            //if (average >= 70 && average < 80)
+            //{
+            //    letterGrade = 'C';
+            //}
+            //if (average >= 60 && average < 70)
+            //{
+            //    letterGrade = 'D';
+            //}
+            //if (average < 60)
+            //{
+            //    letterGrade = 'F';
+            //    passed = false; 
+            //}
+
+            //Console.WriteLine("\n Final Grade Report ");
+            //Console.WriteLine("Average Score: " + average.ToString("F2") + "%");
+            //Console.WriteLine("Letter Grade: " + letterGrade);
+
+            //if (passed == true)
+            //{
+            //    Console.WriteLine("Status: Congratulations! You passed.");
+            //}
+            //if (passed == false)
+            //{
+            //    Console.WriteLine("Status: Retake required.");
+            //}
+
+
+            Greet();
+            Greet();
+            Console.ReadLine();
         }
     }
 }
