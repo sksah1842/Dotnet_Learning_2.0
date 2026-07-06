71b7d942b54fc5fd7e6af6f387212351e65d3af8
sksah <sksah1842@gmail.com>
2026-07-06 21:05:39 +0530

    [Day-10]-Encapsulation and BankAccountSystem

---
+++ BankAccountSystem.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class BankAccount
+    {
+        private string accountHolder;
+        private int accountNumber;
+        private double balance;
+
+        public BankAccount(string name, int accNo, double initialBalance)
+        {
+            accountHolder = name;
+            accountNumber = accNo;
+            balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"{amount} deposited successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid deposit amount.");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Invalid withdrawal amount.");
+            }
+            else if (amount > balance)
+            {
+                Console.WriteLine("Insufficient balance.");
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine($"{amount} withdrawn successfully.");
+            }
+        }
+
+        public void DisplayAccount()
+        {
+            Console.WriteLine("\n----- Account Details -----");
+            Console.WriteLine("Account Holder : " + accountHolder);
+            Console.WriteLine("Account Number : " + accountNumber);
+            Console.WriteLine("Current Balance: " + balance);
+        }
+    }
+    internal class BankAccountSystem
+    {
+        public static void Main(string[] args) {
+
+            BankAccount account = new BankAccount("Sumit", 1001, 5000);
+
+            account.DisplayAccount();
+
+            account.Deposit(2000);
+            account.Withdraw(1500);
+
+            account.DisplayAccount();
+        }
+}}
---
+++ Encapsulation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    //class Stud
+    //{
+    //    private int age;
+
+    //    public void SetAge(int value)
+    //    {
+    //        if (value > 0)
+    //            age = value;
+    //        else
+    //            Console.WriteLine("Invalid Age");
+    //    }
+
+    //    public int GetAge()
+    //    {
+    //        return age;
+    //    }
+    //}
+
+    abstract class Speaker
+    {
+        public abstract void Sound();
+    }
+
+    class JBL : Speaker
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("JBL speaker");
+        }
+    }
+    internal class Encapsulation
+    {
+        public static void Main(string[] args)
+        {
+            //Stud s = new Stud();
+            //s.SetAge(21);
+
+            //Console.WriteLine(s.GetAge());
+
+            JBL j = new JBL();
+            j.Sound();
+
+        }
+    }
+}
