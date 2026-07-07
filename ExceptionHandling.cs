9df76e40da0e60e713b3e0dfee2a0a924080ae93
sksah <sksah1842@gmail.com>
2026-07-07 20:47:27 +0530

    [Day-10]-ExceptionHandling and FileReader

---
+++ ExceptionHandling.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class InvalidAgeException : Exception
+    {
+        public InvalidAgeException(string message)
+            : base(message)
+        {
+        }
+    }
+    internal class ExceptionHandling
+    {
+        public static void Main(string[] args)
+        {
+            //int age = -5;
+
+            //try
+            //{
+            //    if (age < 0)
+            //    {
+            //        throw new Exception("Age cannot be negative.");
+            //    }
+
+            //    Console.WriteLine(age);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            try
+            {
+                int age = -1;
+
+                if (age < 0)
+                    throw new InvalidAgeException("Invalid Age");
+            }
+            catch (InvalidAgeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ FileReader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class FileReader
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter file path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string content = reader.ReadToEnd();
+
+                    Console.WriteLine("\n----- File Content -----");
+                    Console.WriteLine(content);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Error: File not found.");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Error: Directory not found.");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Error: Permission denied.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("I/O Error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unexpected Error: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nProgram Finished.");
+            }
+        }
+    }
+}
